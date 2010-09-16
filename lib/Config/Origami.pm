1410f02d5cd89e5dbebaac4c797d0682070c4203
Ask Bjørn Hansen <ask@develooper.com>
2010-09-15 23:47:28 -0700

    Newly minted distribution

---
+++ lib/Config/Origami.pm
@@ -0,0 +1,51 @@
+package Config::Origami;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Config::Origami - Layered JSON configuration
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over4
+
+=item ...
+
+=back
+
+=head1 AUTHOR
+
+Ask Bjørn Hansen, C<< <ask at develooper.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the issue tracker at
+L<http://github.com/abh/Config-Origami/issues>.
+
+The Git repository is available at
+L<http://github.com/abh/Config-Origami>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Ask Bjørn Hansen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+
+=cut
